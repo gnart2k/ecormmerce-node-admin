48564f853a830fef3cf3a3bc7f617d35687b8693
trang <trangbg2000@gmail.com>
2023-10-24 18:46:32 +0700

    create form with modal

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,19 +1,15 @@
 "use client";
-import { Button } from "@/components/ui/button";
-import { Modal } from "@/components/ui/modal";
-import { UserButton } from "@clerk/nextjs";
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { useEffect } from "react";
 
 export default function Home() {
-  return (
-    <div className="p-4">
-      <Modal
-        title="test"
-        description="modal description"
-        isOpen
-        onClose={() => {}}
-      >
-        Content
-      </Modal>
-    </div>
-  );
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
+  return <div className="p-4"></div>;
 }
