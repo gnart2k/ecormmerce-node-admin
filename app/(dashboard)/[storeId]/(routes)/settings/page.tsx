14781056ed7dc079c95466bf53cafdf977d9e210
trang <trangbg2000@gmail.com>
2023-10-29 16:58:15 +0700

    create navbar and store switcher

---
+++ app/(dashboard)/[storeId]/(routes)/settings/page.tsx
@@ -0,0 +1,3 @@
+export default function SettingPages() {
+  return <div>setting page</div>;
+}
--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -11,5 +11,5 @@ export default function Home() {
       onOpen();
     }
   }, [isOpen, onOpen]);
-  return <div className="p-4"></div>;
+  return null;
 }
