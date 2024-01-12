8278585117c3607a5170ee929a8ce0aba31ccb04
trang <trangbg2000@gmail.com>
2024-01-12 17:25:00 +0700

    add cell action to dashboard

--- app/(dashboard)/[storeId]/(routes)/billboard/components/cell-action.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboard/components/cell-action.tsx
@@ -1,15 +1,91 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { BillboardCollumn } from "./columns";
-import { DropdownMenu } from "@radix-ui/react-dropdown-menu";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "@radix-ui/react-dropdown-menu";
+import { Button } from "@/components/ui/button";
+import { Copy, Edit, MoreHorizontal, Trash } from "lucide-react";
+import toast from "react-hot-toast";
+import { useParams, useRouter } from "next/navigation";
+import axios from "axios";
+import { AlertModal } from "@/components/modals/alert-modal";
 
 interface CellActionProps {
   data: BillboardCollumn;
 }
 export const CellAction: React.FC<CellActionProps> = ({ data }) => {
+  const router = useRouter();
+  const params = useParams();
+  const [loading, setLoading] = useState(false);
+  const [open, setOpen] = useState(false);
+  const onCopy = (id: string) => {
+    navigator.clipboard.writeText(id);
+    toast.success(`copied ${id}`);
+  };
+
+  const onUpdate = () => {
+    router.push(`/${params.storeId}/billboard/${data.id}`);
+  };
+
+  const onDelete = async () => {
+    try {
+      setLoading(true);
+      await axios.delete(`/api/${params.storeId}/billboard/${data.id}`);
+      router.refresh();
+      router.push("/");
+      toast.success("billboard deleted");
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
     <div>
-      <DropdownMenu></DropdownMenu>
+      <AlertModal
+        isOpen={open}
+        onClose={() => setOpen(false)}
+        onConfirm={onDelete}
+        loading={loading}
+      />
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant={"ghost"} className="h-8 w-8 p-0">
+            <span className="sr-only">Open menu</span>
+            <MoreHorizontal className="h-4 w-4" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="center">
+          <DropdownMenuLabel>
+            <b>Action</b>
+          </DropdownMenuLabel>
+
+          <DropdownMenuItem
+            className="flex items-center p-2 bg-gray-100 focus:outline-none focus:cursor-pointer"
+            onClick={() => onCopy(data.id)}
+          >
+            <Copy className="mr-2 h-4 w-4" />
+            Copy Id
+          </DropdownMenuItem>
+          <DropdownMenuItem
+            onClick={() => onUpdate()}
+            className="flex items-center p-2 bg-gray-100 focus:outline-none focus:cursor-pointer"
+          >
+            <Edit className="mr-2 h-4 w-4" />
+            Update
+          </DropdownMenuItem>
+          <DropdownMenuItem
+            onClick={() => setOpen(true)}
+            className="flex items-center p-2 bg-gray-100 focus:outline-none focus:cursor-pointer"
+          >
+            <Trash className="mr-2 h-4 w-4" />
+            Delete
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </div>
   );
 };
